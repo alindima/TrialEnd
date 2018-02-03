2c60b41c695387ee7441155acb2cf57d3f2f9d4b
alindima <alin_dima@yahoo.com>
2018-02-03 21:22:39 +0200

    Add landing page styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,79 +9,31 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
+        <div class="container-fluid">
+            <div class="row text-center">
+                <div class="col-xs-12 links">
+                    <div class="pull-right">
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
-                    @endauth
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    TrialEnd
+            </div>
+            <div class="row row2">
+                <div class="col-md-6 col-md-offset-2 content">
+                    <div class="title">
+                        <h1>TrialEnd</h1>
+                        <p>Tired of getting billed after your free trials expire?<br>
+                            You now can manage all your trial accounts and get notified whenever they expire. It's never been easier.<br>
+                            Just enter the details of every account you have and we'll make sure to send you an email a day before the expiry day, so that you may cancel it in time.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
